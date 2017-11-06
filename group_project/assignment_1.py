3289b3de756ad675000caa26d261f0fa4c2ea5d5
Sara de Souza <saradesouza87@gmail.com>
2017-11-06 15:06:03 +0000

    done with assignment 1 baby!

--- group_project/assignment_1.py
+++ group_project/assignment_1.py
@@ -1,7 +1,18 @@
 def first_half(a_string):
-    pass
-
-
+    stringlength = len(a_string)
+    if stringlength % 2 == 0:
+        #print(stringlength)
+        halfstring = stringlength / 2
+        #print(halfstring)
+        #print(a_string[:halfstring])
+        return(a_string[:halfstring])
+    else:
+        #print(stringlength)
+        halfstring = stringlength / 2
+        #print(halfstring)
+        #print(a_string[:halfstring])
+        return(a_string[:halfstring + 1])
+        
 def test_first_half_with_even_length_string():
     assert first_half('abcdef') == 'abc'
 
