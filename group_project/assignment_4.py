43da9c7befa056e971ad6d9802b7c401fbc9bb2e
Sara de Souza <saradesouza87@gmail.com>
2017-11-09 15:39:24 +0000

    assignment 4 done

--- group_project/assignment_4.py
+++ group_project/assignment_4.py
@@ -1,6 +1,12 @@
 def how_many_times(a_string, a_word):
-    pass
-
+    if a_word in a_string:
+        print(a_string[-13:-7])
+        if a_string[-13:-7] == 'Python':
+            return 2
+        else:
+            return 1
+    else:
+        return 0
 
 def test_more_than_once():
     phrase = "Python is a great language. I like Python a lot."
