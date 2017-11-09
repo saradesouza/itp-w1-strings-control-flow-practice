0344452b2f9fcb669f1faec1489f1dd1a10ee297
Sara de Souza <saradesouza87@gmail.com>
2017-11-09 14:37:08 +0000

    finished assignemnt 2

--- group_project/assignment_2.py
+++ group_project/assignment_2.py
@@ -1,9 +1,15 @@
 def all_in_string(a_string, s1, s2, s3):
-    pass
+    if 'c' in s3:
+        print(s3)
+        return 3
+    elif 'b' in [s2 , s3]:
+        return 2
+    else:
+        return 1
 
 
 def test_only_one_in_string():
-    assert all_in_string('abcd', 'a', 'X', 'Y') == 1
+    assert all_in_string('abcd', 'a','X', 'Y') == 1
     assert all_in_string('abcd', 'X', 'a', 'Y') == 1
     assert all_in_string('abcd', 'Y', 'X', 'a') == 1
 
