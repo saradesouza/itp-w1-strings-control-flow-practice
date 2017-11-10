103fdd3789c635a3d7192ce22f3d995f80a31f19
Sara de Souza <saradesouza87@gmail.com>
2017-11-10 14:44:09 +0000

    ugh I give up

--- group_project/assignment_6.py
+++ group_project/assignment_6.py
@@ -1,4 +1,4 @@
-# Example. Use it as a base to build your own template.
+#Example. Use it as a base to build your own template.
 BOARD_TEMPLATE = """
 O  |  O  |  X
 --------------
@@ -9,8 +9,41 @@ O  |  X  |  O
 
 
 def format_tic_tac_toe_board(first_row, second_row, third_row):
-    pass
-
+    #first, I spliced each row's letter into each own variable
+    a1 = first_row[0]
+    a2 = first_row[1]
+    a3 = first_row[2]
+    b1 = second_row[0]
+    b2 = second_row[1]
+    b3 = second_row[2]
+    c1 = third_row[0]
+    c2 = third_row[1]
+    c3 = third_row[2]
+    
+    #so somehow I need to insert them like this
+    #a1  |  a2  |  a3
+    #--------------
+    #b4  |  b5  |  b6
+    #--------------
+    #c1  |  c2  |  c3
+    
+    #I decided to make other things into its own variable
+    line = "--------------"
+    triplequote = "\"\"\""
+    
+    #I diligently built each line into its own variable
+    line1 = a1 + "  |  " + a2 + "  |  " + a3
+    line2 = b1 + "  |  " + b2 + "  |  " + b3
+    line3 = c1 + "  |  " + c2 + "  |  " + c3
+    
+    #this actually makes a multiline in print!
+    multilinehack = """
+    """
+    #so I mash all the variables together
+    myboard = triplequote + multilinehack + line1 + multilinehack + line + multilinehack + line2 + multilinehack + line + multilinehack + line3 + multilinehack + triplequote
+    
+    print(myboard) #it looks like it prints perfectly!
+    return myboard #but it comes out as a mess as a result?!
 
 def test_format_board():
     """
